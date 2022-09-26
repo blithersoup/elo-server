c512086cb619db9e04c22e021af6b42b29eb3b93
Grady Arnold <gradyarnold11@gmail.com>
2022-09-26 19:08:45 -0400

    requirements

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: cd frontend && npm start
+web: cd frontend && npm run build && npm run start
 server: python backend/app.py
--- backend/__init__.py
+++ backend/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from flask_jwt_extended import JWTManager
+#from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 import os
 import socket
 
-username = os.environ.get('USER')
-password = os.environ.get('DBPASS')
-tablename= os.environ.get('TABLENAME')
-
+#username = os.environ.get('USER')
+#password = os.environ.get('DBPASS')
+#tablename= os.environ.get('TABLENAME')
+uri = os.environ.get('URI')
 
 #key = os.environ.get('SECRETKEY')
 ip = socket.gethostbyname(socket.gethostname())
@@ -18,7 +18,8 @@ app = Flask(__name__)
 CORS(app)
 
 #app.config["JWT_SECRET_KEY"] = key
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{username}:{password}@localhost/{tablename}'
+#app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{username}:{password}@localhost/{tablename}'
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
