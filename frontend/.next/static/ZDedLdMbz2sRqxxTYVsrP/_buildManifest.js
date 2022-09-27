24522d67428359505fd29fa48ff5ecec46c22df4
Grady Arnold <gradyarnold11@gmail.com>
2022-09-26 23:10:41 -0400

    git move

--- frontend/.next/static/ZDedLdMbz2sRqxxTYVsrP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/988-d3baa34dc55bd581.js","static/chunks/pages/index-224627c9cbd98c77.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
